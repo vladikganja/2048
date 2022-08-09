2eab854de53cfea541582f02b0d087763861ad6a
Vladislav Kurylev <vlad210746@gmail.com>
2022-08-09 10:37:25 +0300

    run fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,11 @@
 #include "Game.h"
 
 int main(int argc, char** argv) {
-
     sf::RenderWindow window(sf::VideoMode(Utils::WINDOW_WIDTH, Utils::WINDOW_HEIGHT), "2048", sf::Style::Close);
 
     Utils::font.loadFromFile("../../multimedia/CyrilicOld.TTF");
 
-    Game game(argc > 1 ? static_cast<int>(*argv[1]) : 4);
+    Game game(argc > 1 ? *argv[1] - 48 : 4);
     game.mainloop(window);
 
     return 0;
